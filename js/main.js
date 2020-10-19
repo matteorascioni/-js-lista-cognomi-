8d8857c863765fe9c61073b33af93f320a9a4a2e
Matteo <Rascioni>
2020-10-19 18:15:20 +0200

    90%

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+// lISTA DEI COGNOMI 
+var list = [ 
+    'Bianchi',
+    'Rossi',
+    'Duzioni',
+    'Balsano',
+    'Giorgini',
+    'Rossi',
+    'Da Vinci',
+    'Rascioni'];
 
-var list = [ 'Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini', 'Rossi', 'Da Vinci', 'Rascioni']
+// ORDINE ALFABETICO
+list.sort();
 
-    function myFunction() {
-        list.sort();
-        document.getElementById("surnames-list").innerHTML = list;
-      }
+// LOOP FOR
+for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+
+    // OUTPUT
+    var items = document.getElementById('surnames-list').innerHTML;
+    document.getElementById('surnames-list').innerHTML = items + '<li>' + item + '<li>';
+}
+
+
